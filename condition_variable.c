1991c3a55eaa5df197072e7d5be72bcb92debb50
MasterTos <MasterTos@yahoo.com>
2017-04-10 16:00:01 +0700

    edit main

--- condition_variable.c
+++ condition_variable.c
@@ -28,6 +28,7 @@ void process_msg(void) {
         workq = mp->m_next;
         pthread_mutex_unlock(&qlock);
         /* now process the message mp */
+        pthread_exit((void *)2);
     }
 }
 
@@ -39,7 +40,30 @@ void enqueue_msg(struct msg *mp) {
     pthread_cond_signal(&qready);
 }
 
+void loop_enqueue_msg(void) {
+    int i;
+    for(i = 1 ; i <= 50 ; i++) {
+        
+    }
+    pthread_exit((void *)2);
+}
 
 int main() {
-
+    pthread_t tid1, tid2;
+    void *tret;
+    error = pthread_create(&tid1, NULL, process_msg, NULL);
+    if (error)
+        err_exit(error, "can’t create thread 1");
+    error = pthread_create(&tid2, NULL, loop_enqueue_msg, NULL);
+    if (error)
+        err_exit(error, "can’t create thread 2");
+    error = pthread_join(tid1, &tret);
+    if (error)
+        err_exit(error, "can’t join with thread 1");
+    printf("thread 1: exit code %ld\n", (long)tret);
+    error = pthread_join(tid2, &tret);
+    if (error)
+        err_exit(error, "can’t join with thread 2");
+    printf("thread 2: exit code %ld\n", (long)tret);
+    exit(EXIT_SUCCESS);
 }
